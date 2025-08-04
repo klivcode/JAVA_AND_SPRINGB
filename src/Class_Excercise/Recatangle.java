d37f02a5db11d2659e243ce0bc02e2ab2c3a48f6
Bhim Raj Bhandari <infobrb04@gmail.com>
2025-08-04 18:38:35 +0545

    Update the code Class||Rectangle

--- src/Class_Excercise/Recatangle.java
+++ src/Class_Excercise/Recatangle.java
@@ -5,4 +5,28 @@ import java.util.Scanner;
 public class Recatangle {
 
 
+
+
+    static  int getArea(int len, int bred)
+    {
+        return (len * bred) ;
+    }
+    static int getDim(){
+        System.out.println("Enter Length and Breadth");
+        Scanner scanner = new Scanner(System.in);
+        int len = scanner.nextInt();
+        int bred = scanner.nextInt();
+        int res=getArea(len, bred);
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        Recatangle recatangle = new Recatangle();
+        int fin= recatangle.getDim();
+        System.out.println(fin);
+    }
+
+
+
 }
