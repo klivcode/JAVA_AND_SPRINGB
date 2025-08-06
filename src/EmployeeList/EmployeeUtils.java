1da9f6c642cdbd9c94e82eddaddc93b85cd3a420
Bhim Raj Bhandari <infobrb04@gmail.com>
2025-08-06 05:56:43 +0545

    CHnaged

--- src/EmployeeList/EmployeeUtils.java
+++ src/EmployeeList/EmployeeUtils.java
@@ -8,22 +8,25 @@ public class EmployeeUtils {
 
     public Employee employeeWithHighestSalary(List<Employee> employeeList) {
 
-
+        //TODO
         return employeeList.get(0);
 
     }
 
     public Employee employeeWithLowestSalary(List<Employee> employeeList) {
+        //TODO
     return employeeList.get(0);
     }
 
 
     public Employee sumOfEmployeeSalary(List<Employee> employeeList) {
+        //TODO
     return employeeList.get(0);
     }
 
     public Map<String,Integer> employeeCountBasedOnDepartment(List<Employee> employeeList)
     {
+        //TODO
        return new HashMap<String,Integer>();
     }
 }
